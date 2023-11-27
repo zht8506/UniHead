8b400ddb6b646e0f439c30288f71164c84e222ec
zht8506 <81303574+zht8506@users.noreply.github.com>
2023-11-27 09:51:25 +0800

    Update dist_test.sh

--- dist_test.sh
+++ dist_test.sh
@@ -15,7 +15,7 @@ python -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/test_mm.py \
+    $(dirname "$0")/tools/test.py \
     $CONFIG \
     $CHECKPOINT \
     --format-only \
